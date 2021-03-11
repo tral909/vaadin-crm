daa058d56c53e6d67747be399d3d93540c4fae61
tral909 <indorm1992@mail.ru>
2021-03-11 10:37:00 +0300

    Add grid with contacts

---
+++ src/main/java/com/vaadin/tutorial/crm/Application.java
@@ -0,0 +1,16 @@
+package com.vaadin.tutorial.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+public class Application {//extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
